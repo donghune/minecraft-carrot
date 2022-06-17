0b3a13c385dbabb9997a7e9ea1f05a411e895477
donghune <reona7140@naver.com>
2022-06-17 11:08:09 +0900

    fix gradle.yml

--- carrot-plugins/build.gradle.kts
+++ carrot-plugins/build.gradle.kts
@@ -5,15 +5,18 @@ subprojects {
         dependsOn(tasks.processResources)
         archiveFileName.set("${project.name}.jar")
         doLast {
+            val sep = File.separator
             copy {
-                val sep = File.separator
                 from("${buildDir.absolutePath}${sep}libs$sep${project.name}.jar")
                 into("${pluginProject.rootDir.absolutePath}${sep}.server-mod${sep}plugins${sep}")
             }
             copy {
-                val sep = File.separator
                 from("${buildDir.absolutePath}${sep}libs$sep${project.name}.jar")
                 into("${pluginProject.rootDir.absolutePath}${sep}.server${sep}plugins${sep}")
+            }
+            copy {
+                from("${buildDir.absolutePath}${sep}libs$sep${project.name}.jar")
+                into("${pluginProject.rootDir.absolutePath}${sep}releases${sep}")
             }
         }
     }
